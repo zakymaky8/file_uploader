9fcf7819901883c8e65838d9ff9c41d82667f614
Gizachew Meku <zekaryasmekuaninit@gmail.com>
2024-11-16 23:36:30 +0300

    modify packe.json file

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const path = require("path");
+const multer = require("multer");
+// const upload = multer({dest: "/upload/"});
 
 const app = express();
 
@@ -7,3 +9,54 @@ app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
 app.use(express.urlencoded({extended: false}));
+app.use(express.static("views"))
+
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        console.log(file);
+
+        cb(null, "uploads/");
+    },
+    filename: (req, file, cb) => {
+        console.log(file)
+        cb(null, (new Date().toLocaleDateString()).split("/").join("") + "" + file.originalname)
+    }
+})
+
+const upload = multer({storage: storage})
+
+app.get("/", (req, res) => {
+    res.render("index")
+})
+
+app.post("/upload", upload.single('file') , (req, res) => {
+    const file = req.file;
+    res.send("file uploaded successfully file name: " + file.filename + "  path: " + file.path)
+})
+
+
+
+app.listen(8081, () => {
+    console.log("server is listening on port 8081");
+})
+
+
+
+// app.use(
+//     expressSession({
+//       cookie: {
+//        maxAge: 7 * 24 * 60 * 60 * 1000 // ms
+//       },
+//       secret: 'a santa at nasa',
+//       resave: true,
+//       saveUninitialized: true,
+//       store: new PrismaSessionStore(
+//         new PrismaClient(),
+//         {
+//           checkPeriod: 2 * 60 * 1000,  //ms
+//           dbRecordIdIsSessionId: true,
+//           dbRecordIdFunction: undefined,
+//         }
+//       )
+//     })
+//   );
