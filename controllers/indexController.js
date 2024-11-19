ed38c4009b597fb96279b1d49b75c259993bce07
Gizachew Meku <zekaryasmekuaninit@gmail.com>
2024-11-19 20:55:16 +0300

    create editing deleting and detail logic

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -1,8 +1,10 @@
-const { redirect } = require("react-router-dom")
 const File = require("../models/fileModel")
 const Folder = require("../models/folderModel")
 const User = require("../models/userModel")
 const { PrismaClient } = require("@prisma/client");
+const fs = require("fs")
+const path = require("path")
+const { validationResult } = require("express-validator");
 const prisma = new PrismaClient();
 
 const getHome = async (req, res) => {
@@ -25,12 +27,23 @@ async function getArr(folder) {
     return list.reverse()
 }
 
+async function getInRootFolderItem(folder) {
+    for (let i=0; i<100; i++) {
+        if (folder.parentId) {
+            folder = folder = await prisma.folder.findFirst({where: {folders_id: folder.parentId}});
+        } else {
+            return {name: folder.folder_name, id: folder.folders_id}
+        }
+    }
+}
+
 const getToDataInsideFolder = async (req, res) => {
     const { folder_id } = req.params;
     const allData = await Folder.getAllDataInsideFolder(req.user, folder_id);
     const curFol = await prisma.folder.findFirst({where: {folders_id: folder_id}});
+    const inrootFolder = await getInRootFolderItem(curFol)
     const path = await getArr(curFol);
-    console.log(path)
+    path.unshift(inrootFolder);
     res.render("index", {creationEndpt: folder_id, user: req.user, data: allData, path: path});
 }
 
@@ -46,6 +59,11 @@ const signUserOut =  (req, res) => {
 }
 
 const registerUser = async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        res.render("signup", {errors: errors.array(), fieldValues: req.body});
+        return;
+    }
     await User.createUser(req.body);
     res.redirect("/login")
 }
@@ -76,6 +94,42 @@ const createNewFile = async (req, res) => {
     }
 }
 
+const editFolderName = async (req, res) => {
+    const { folder_id } = req.params;
+    await Folder.updateFolder(req.user, folder_id, req.body);
+    const folderToBeEdited = await prisma.folder.findFirst({where: {user_id: req.user.users_id, folders_id: folder_id}});
+    const parentFoldr = folderToBeEdited.parentId ? await prisma.folder.findFirst({where: {user_id: req.user.users_id, folders_id: folderToBeEdited.parentId}}) : "";
+    res.redirect(folderToBeEdited.parentId ? `/folder/${parentFoldr.folders_id}` : "/")
+}
+
+
+const deleteSingleFile = async (req, res) => {
+    const { file_id } = req.params;
+    const fileTobeDeleted = await prisma.file.findFirst({where:{files_id: file_id}})
+    const parentFolder = fileTobeDeleted.folder_id ? await prisma.folder.findFirst({where: {folders_id: fileTobeDeleted.folder_id}}) : null;
+    await File.deleteFile(file_id, req.user);
+    res.redirect( parentFolder ? `/folder/${parentFolder.folders_id}` : "/")
+}
+
+const fileDetailGet = async (req, res) => {
+    const { file_id } = req.params;
+    const fileDetail = await File.getFileDetail(req.user, file_id)
+    res.render("fileDetail", {fileDetail: fileDetail})
+}
+
+const dowloadFile = async (req, res) => {
+    const file = await prisma.file.findFirst({where: {files_id: req.params.file_id}})
+    const filePath = path.join("/home/zeki/repos/file_uploader/uploads", file.filename)
+
+    if (fs.existsSync(filePath)) {
+        res.download(filePath)
+        return;
+    } else {
+        res.send("file not found")
+        return 
+    }
+}
+
 module.exports = {
     getHome,
     getLoginForm,
@@ -84,5 +138,9 @@ module.exports = {
     signUserOut,
     createFolder,
     createNewFile,
-    getToDataInsideFolder
+    getToDataInsideFolder,
+    editFolderName,
+    deleteSingleFile,
+    fileDetailGet,
+    dowloadFile
 }
